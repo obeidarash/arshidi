9c41d0f8f2e239fa3d07dedbe9fc4b1d92ab0757
Obeid Arash <obeid75arash@gmail.com>
2022-10-29 05:56:11 +0330

    fix bug in Library Model

--- arshidipedia/models.py
+++ arshidipedia/models.py
@@ -6,6 +6,12 @@ from django.db.models.signals import post_delete, pre_save, post_save
 from management.models import Employee
 from tinymce.models import HTMLField
 
+CATEGORY = [
+    ('web-development', 'Web Development'),
+    ('seo', 'SEO'),
+    ('wordpress', 'Wordpress'),
+]
+
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -35,10 +41,11 @@ class File(models.Model):
 
 class Library(models.Model):
     title = models.CharField(max_length=512)
+    category = models.CharField(choices=CATEGORY, default=CATEGORY[0], max_length=128)
     file = models.FileField(upload_to=upload_file_path, null=True, blank=True,
                             help_text="Upload your pdf, docx, .... file in here")
     link = models.URLField(blank=True, null=True)
-    content = HTMLField(null=True, blank=True)
+    content = HTMLField()
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
 
@@ -64,7 +71,7 @@ def post_save_expense(sender, instance, *args, **kwargs):
 def pre_save_expense(sender, instance, *args, **kwargs):
     """ instance old image file will delete from os """
     try:
-        old_img = instance.__class__.objects.get(id=instance.id).attach.path
+        old_img = instance.__class__.objects.get(id=instance.id).file.path
         try:
             new_img = instance.file.path
         except:
