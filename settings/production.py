5401e8ad45fb82d714ae0c1a6131dd7172ebc687
Obeid Arash <obeid75arash@gmail.com>
2022-12-04 16:37:26 +0330

    change settings in produsction mode for deployment

--- settings/production.py
+++ settings/production.py
@@ -8,9 +8,9 @@ ALLOWED_HOSTS = ['arshidi.com', 'www.arshidi.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'isitnowr_arshididatabase',
-        'USER': 'isitnowr_arshidiusername',
-        'PASSWORD': 'jTudjgNk7eHH',
+        'NAME': 'isitnowr_thisisthedatabaseofarshidicrm',
+        'USER': 'isitnowr_usernameofarshidicrmwebapp',
+        'PASSWORD': '[@cnY.gkFv&d(kM8Pp',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
