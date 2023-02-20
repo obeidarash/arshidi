8ddab27540ffde0c6680b5c5c5674b12ba39de71
Obeid Arash <obeid75arash@gmail.com>
2023-02-20 20:55:03 +0330

    add link to conatcts in list_display of companies

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Contact, Company, Tag
 from .forms import ContactAdminForm
+from django.utils.html import format_html
+
 
 
 @admin.register(Tag)
@@ -26,11 +28,11 @@ class ContactsAdmin(admin.ModelAdmin):
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'contacts','tag',)
     autocomplete_fields = ('contact', 'tags',)
-    search_fields = ('name',)
+    search_fields = ('name', )
     list_filter = ('tags',)
 
     def tag(self, obj):
         return ",\n".join([p.title for p in obj.tags.all()])
 
     def contacts(self, obj):
-        return ",\n".join([f'{p.firstname} {p.lastname}' for p in obj.contact.all()])
+        return format_html(",\n".join([f'<a href="/admin/contacts/contact/{p.id}/change/">{p.firstname} {p.lastname}</a>' for p in obj.contact.all()]))
